02017ed8fcf451d5131b8563bd8d8bfc006b8d68
윤희찬 <yhc920923@gmail.com>
2022-12-11 15:46:52 +0900

    컴포넌트 분리작업

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer py-4">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-4 text-lg-start">한양인팩</div>
+        <div class="col-lg-4 my-3 my-lg-0">
+          <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+          <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+          <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+        <div class="col-lg-4 text-lg-end">
+          <div class="text-decoration-none">
+            경기도 김포시 승가로 76번길 29 대표 윤도상
+          </div>
+          <div class="text-decoration-none">
+          </div>
+          <div class="text-decoration-none">
+            경기도 김포시 승가로 76번길 29
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
