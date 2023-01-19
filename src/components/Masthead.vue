e58c3bcc4c871c52e7b59bcca9f6a8c421e98809
Heechan Yoon <yhcyoon@nara.co.kr>
2023-01-19 17:42:49 +0900

    css

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="masthead">
     <div class="container">
-      <div class="masthead-subheading">Welcome To Our Studio!</div>
-      <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-      <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+      <div class="masthead-heading text-uppercase">최고의 제품을 최고의 가치로</div>
+      <div class="masthead-subheading">고객의 소중한 제품에 걸맞는 최고의 상자를 만들어드립니다</div>
+      <a class="btn btn-primary btn-xl text-uppercase" href="#contact">견적 문의</a>
     </div>
   </header>
 </template>
