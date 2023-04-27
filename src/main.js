4b2bbb307f3c97b885b452fc85354334150f26a8
Yoon HeeChan <yhc920923@gmail.com>
2023-04-27 18:02:17 +0900

    card

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8081'
 
-createApp(App).use(store).use(router).mount('#app')
+// app 생성
+const app = createApp(App)
+
+// axios 전역사용
+app.config.globalProperties.$axios = axios
+
+// store 사용 설정
+// router 사용 설정
+// main id 가진 곳에 app 마운트
+app.use(store).use(router).mount('#app')
